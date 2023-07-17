29c8207ba9a58f67dc82e8b5f27f0d4581f163b1
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-07-17 05:54:39 +0300

    add autoload

--- confidentiality-mode.el
+++ confidentiality-mode.el
@@ -7,6 +7,7 @@
 (defvar confidentiality-mode-display-type 'sentence
   "The type of text to display. Possible values are 'sentence' and 'paragraph'.")
 
+;;;###autoload
 (define-minor-mode confidentiality-mode
   "Randomize text in lines other than the current line."
   :init-value nil
